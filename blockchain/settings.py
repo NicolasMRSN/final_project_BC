87746c94ce1eaf82d0bc0478921a76b4ab2ba592
Malte Hedderich <hedderich@Not-Your-MacBook.local>
2019-12-19 15:58:33 +0800

    added Bootstrap design

--- blockchain/settings.py
+++ blockchain/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blockchain.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['blockchain/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
