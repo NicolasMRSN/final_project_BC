f5dce18d90825a1db69fe1374a1c58b1e6d0d634
Malte Hedderich <hedderich@Not-Your-MacBook.local>
2019-12-20 17:20:58 +0800

    delete models.py

--- authentication/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    """Application user database.
-    """
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    wallet_id = models.CharField(max_length=42)
